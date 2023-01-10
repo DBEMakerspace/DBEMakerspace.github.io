591f4cf9d563d2f4fefcf5844c95cdb5942dbda9
CamWhiting <cameron.whiting@hotmail.com>
2023-01-10 12:41:30 +0800

    Fixed Tina.io V1
    Fixed functionality to edit pages but still need to make process more clear.

--- .tina/config.js
+++ .tina/config.js
@@ -6,23 +6,23 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
   branch,
   clientId: "5a6d2ab5-c8e5-46e5-a1dc-8a8f46fd9c6a", // Get this from tina.io
-  token: null, // Get this from tina.io
+  token: "a6a2fcc5f60525115a773235620e8b21c8fafd2e", // Get this from tina.io
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "static",
   },
   media: {
     tina: {
       mediaRoot: "",
-      publicFolder: "public",
+      publicFolder: "static",
     },
   },
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: "exhibitions",
+        label: "Exhibitions",
+        path: "_exhibitions",
         fields: [
           {
             type: "string",
