6014120d444bab323913aeac695654b48d3ff760
DBEMakerspace <116771511+DBEMakerspace@users.noreply.github.com>
2023-01-10 12:19:00 +0800

    Added Token

--- .tina/config.js
+++ .tina/config.js
@@ -6,7 +6,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
   branch,
   clientId: "5a6d2ab5-c8e5-46e5-a1dc-8a8f46fd9c6a", // Get this from tina.io
-  token: null, // Get this from tina.io
+  token: "a6a2fcc5f60525115a773235620e8b21c8fafd2e", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
